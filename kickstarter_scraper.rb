3263f461301d3c5f764bde8ee6497c211357351a
Monica Gerard <mlgerardvla@gmail.com>
2020-03-26 22:47:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,7 @@
 require "nokogiri"
 require "pry"#
 
+<<<<<<< HEAD
 # #projects: kickstarter.css("li.project.grid_4")
 # #title:  project.css("h2.bbcard_name strong a").text
 # #image: project.css("div.project-thumbnail a img").attribute("src").value
@@ -25,6 +26,15 @@ require "pry"#
 #   end
 #   projects
 # end
+=======
+#projects: kickstarter.css("li.project.grid_4")
+#title:  project.css("h2.bbcard_name strong a").text
+#image: project.css("div.project-thumbnail a img").attribute("src").value
+#location: project.css("span.location-name").text
+#funded:  project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+def create_project_hash
+  html = File.read("fixtures/kickstarter.html")#
+>>>>>>> 4cd55bcb14e2a76dfa6f63b5b7a6171a406fe240
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
@@ -32,6 +42,7 @@ def create_project_hash
 
   projects = {}
 
+<<<<<<< HEAD
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     # binding.pry
@@ -46,4 +57,16 @@ def create_project_hash
   end
 
   projects
+=======
+  #iterate through projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    project[title.to_sym] = {}
+
+    projects
+  end
+
+>>>>>>> 4cd55bcb14e2a76dfa6f63b5b7a6171a406fe240
 end
+
+create_project_hash
